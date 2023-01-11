65d6fcf6c0b42ce952736baede4908bb614e6325
Christian Bingman <christia@meraki.com>
2023-01-11 15:24:55 -0600

    Updated pass and ssh config

--- .config/.raycast/pass.sh
+++ .config/.raycast/pass.sh
@@ -9,6 +9,12 @@
 # @raycast.icon 🔐
 # @raycast.argument1 { "type": "text", "placeholder": "Path" }
 
+PASS="$(gpg -d ~/.password-store/christia.gpg)"
+STATUS=$?
+if [ $STATUS -ne 0 ]; then
+  killall gpg-agent
+fi
+
 if pass -c "$1" 2> /dev/null ; then
     echo "Password Copied to Clipboard"
 else
